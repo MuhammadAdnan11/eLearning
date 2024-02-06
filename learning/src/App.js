3c3810303818a64afa08c63a9570782ea0f3f020
MuhammadAdnan11 <adnan191022@gmail.com>
2024-02-06 13:07:55 +0500

    updated

--- learning/src/App.js
+++ learning/src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
-import Navbar from "./components/Navbars/Navbar";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        {/* <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/service" element={<Service />} />
+
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="*" element={<Error />}></Route>
+        </Routes> */}
+        {/* <Footer /> */}
+      </BrowserRouter>
+    </>
   );
 }
 
